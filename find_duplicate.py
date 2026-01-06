01ff6dbbafb7dc4d51d7d7b71bb9e8250dc622f9
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2026-01-06 16:20:04 +0100

    find_duplicate.py

---
+++ find_duplicate.py
@@ -0,0 +1,12 @@
+def find_duplicates(nums):
+	seen = set()
+	duplicates = set()
+
+	for num in nums:
+		if num in seen:
+			duplicates.add(num)
+		seen.add(num)
+
+	return list(duplicates)
+
+print(find_duplicates([1, 2, 3, 4, 2, 1, 2, 5]))
