418b0c242cd410e9a56a2deaf80995303a864434
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2026-01-07 05:33:56 +0100

    review

--- find_duplicate.py
+++ find_duplicate.py
@@ -1,14 +1,17 @@
+#TODO: Write a function find_duplicates(nums) that takes a list of integers and returns a list of all the
+# duplicate numbers (each duplicate should appear only once in the result).
 
 # Using the set method
 def find_duplicates(nums):
 	seen = set()
-	duplicates = set()
+	duplicate = set()
 
 	for num in nums:
 		if num in seen:
-			duplicates.add(num)
+			duplicate.add(num)
 		seen.add(num)
 
-	return list(duplicates)
+	return duplicate
+
 
 print(find_duplicates([1, 2, 3, 4, 2, 1, 2, 5]))
