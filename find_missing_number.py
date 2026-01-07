e2cffb7bf0c775b22ae2368d85f8f19eecec3d12
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2026-01-07 05:44:10 +0100

    review

--- find_missing_number.py
+++ find_missing_number.py
@@ -1,8 +1,15 @@
+#TODO 1: Write a function find_missing_number(nums) that takes a list of integers from 0 to n with one number missing
+# and returns the missing number.
+
+
 def find_missing_number(nums):
-	n = len(nums)
-	expected_sum = n * (n + 1) // 2
-	actual_sum = sum(nums)
-	return expected_sum - actual_sum
+	length_of_nums = len(nums)
+	expected_sum_of_nums = length_of_nums * ( length_of_nums + 1 ) // 2
+	sum_of_nums = sum(nums)
+
+	missing_num =  expected_sum_of_nums - sum_of_nums
+
+	return missing_num
 
 nums = [3, 0, 1]
 
