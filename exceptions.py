377147adb922a4911175c11691e67a7ba6d08a2b
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2026-02-15 03:37:02 +0100

    practice overflowError exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,1 +1,16 @@
-print("alpha"[0])
+# This code cannot be terminated
+# by pressing Ctrl-C.
+
+from time import sleep
+
+seconds = 0
+
+
+while True:
+    try:
+        print(seconds)
+        seconds += 1
+        sleep(1)
+    except KeyboardInterrupt:
+        print("Interrupted!")
+        raise
